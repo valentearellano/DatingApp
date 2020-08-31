7a27fc19adb4726d06e8ed210d93ba2c6055a80c
Valente Arellano <valente@steeleconsult.com>
2020-08-31 15:05:29 -0600

    End of Section 8

--- DatingApp.API/DTOs/UserForDetailedDto.cs
+++ DatingApp.API/DTOs/UserForDetailedDto.cs
@@ -1,21 +1,18 @@
 using System;
 using System.Collections.Generic;
+using DatingApp.API.Models;
 
-namespace DatingApp.API.Models
+namespace DatingApp.API.DTOs
 {
-    public class User
+    public class UserForDetailedDto
     {
         public int Id { get; set; }
 
         public string Username { get; set; }
 
-        public byte[] PasswordHash { get; set; }
-
-        public byte[] PasswordSalt { get; set; }
-
         public string Gender { get; set; }
 
-        public DateTime DateOfBirth { get; set; }
+        public int Age { get; set; }
 
         public string KnownAs { get; set; }
 
@@ -27,12 +24,15 @@ namespace DatingApp.API.Models
 
         public string LookingFor { get; set; }
 
-        public string Interests { get; set; }
+        public string Interests {get; set; }
 
         public string City { get; set; }
 
         public string Country { get; set; }
 
-        public ICollection<Photo> Photos { get; set; }
+        public string PhotoUrl { get; set; }
+
+        public ICollection<PhotosForDetailedDto> Photos { get; set; }
+
     }
 }
